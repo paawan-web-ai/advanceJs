c3011cf327d9cb6807f7f2cc161de890318e1b54
paawan-web-ai <matrixescape345@gmail.com>
2026-04-10 00:24:24 +0530

    toastercomplete

--- yt/scope/tosternotification/script.js
+++ yt/scope/tosternotification/script.js
@@ -2,17 +2,22 @@ function createToaster(config) {
   return function (str) {
     const div = document.createElement("div");
     div.textContent = str;
-    div.className =
-      "inline-block space-y-3 z-50 bg-blue-500 px-6 py-3 rounded text-white font-bold shadow-lg pointer-events-none transition-opacity duration-300";
+    div.className = `inline-block space-y-3 z-50 ${config.theme === "dark" ? "bg-gray-800 text-white" : "bg-gray-100 text-black"} px-6 py-3 rounded  font-bold shadow-lg pointer-events-none`;
     document.querySelector(".parent").appendChild(div);
+
+    if (config.postionX !== "left" || positionY !== "top") {
+      //   document.querySelector(".parent").classList.add("fixed");
+      document.querySelector(".parent").className +=
+        `${config.positionX === "right" ? " right-5" : " left-5"} ${config.positionY === "bottom" ? " bottom-5" : "top-5"} `;
+    }
 
     setTimeout(() => {
       document.querySelector(".parent").removeChild(div);
-    }, config.duration * 1000);
+    }, config.duration * 2000);
   };
 }
 let toster = createToaster({
-  postionX: "right",
+  positionX: "left",
   positionY: "top",
   theme: "dark",
   duration: 3,
